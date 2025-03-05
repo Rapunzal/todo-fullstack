d849e500dd110d921bb2215c1c2f7dc0ffdb691c
Priyanka <priyankagjune@gmail.com>
2025-03-05 15:09:07 -0700

    cors

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ import Todo from "./models/todoModel.js";
 
 const app = express();
 const port = 8080;
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/todos", async (req, res) => {
   try {
