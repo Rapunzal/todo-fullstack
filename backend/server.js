ac07a80bb00935627c41e92a2b4c42d3943f2d99
Priyanka <priyankagjune@gmail.com>
2025-03-05 09:52:09 -0700

    connecting backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8080;
-
-app.get("/", (req, res) => {
+app.use(cors());
+app.get("/test", (req, res) => {
   res.json("Hello World!");
 });
 
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  async function getData() {
+    const response = await fetch("http://localhost:8080/test");
+    const data = await response.json();
+    console.log(data);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <>hey</>;
 }
 
 export default App;
