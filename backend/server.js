97c5fac2c18afe6a547e88aea1a823915e34175f
Priyanka <priyankagjune@gmail.com>
2025-03-05 11:17:23 -0700

    created post request frontend

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,19 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDb from "./db.js";
+import Todo from "./models/todoModel.js";
 
 const app = express();
 const port = 8080;
 app.use(cors());
-app.get("/test", (req, res) => {
-  res.json("Hello World!");
+app.get("/api/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.status(200).json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
 });
 
 app.listen(port, () => {
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  async function getData() {
-    const response = await fetch("http://localhost:8080/test");
-    const data = await response.json();
-    console.log(data);
+  const [todos, setTodos] = useState([]);
+  const textRef = useRef();
+  async function getTodos() {
+    const response = await fetch("http://localhost:8080/api/todos");
+    const result = await response.json();
+    setTodos(result);
   }
   useEffect(() => {
-    getData();
+    getTodos();
   }, []);
-
-  return <>hey</>;
+  async function handleSubmit(e) {
+    e.preventDefault(); //not refresh page
+    console.log("handleSubmit");
+    console.log(textRef.current.value);
+    const todo = {
+      text: textRef.current.value,
+    };
+    setTodos([...todos, todo]);
+    const response = await fetch("http://localhost:8080/api/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    console.log(response);
+  }
+  return (
+    <>
+      <h1>Todos</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={textRef} />
+        <button>Submit</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
